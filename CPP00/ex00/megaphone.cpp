584c80a65d95587e43dc80b30ddfbedd73cecd56
Corentin Luby <cluby@k2r3p2.42mulhouse.fr>
2024-11-29 17:40:37 +0100

    added an ascii art

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 
-void	strprint_upper(char * str)
+void	strprint_upper(std::string str, std:: locale loc)
 {
-	while (*str)
-	{
-		std::cout << (unsigned char)std::toupper(*str++);
-	}
+	for (std::string::size_type i = 0; i < str.length(); i++)
+		std::cout << std::toupper(str[i], loc);
 }
 
 int	main(int argc, char **argv)
 {
+	std::locale loc;
+
 	if (argc < 2)
 		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n" , 1);
 	for (int i = 1; i < argc; i++)
-	{
-		strprint_upper(argv[i]);
-	}
+		strprint_upper(argv[i], loc);
 	std::cout << '\n';
 	return (0);
 }
